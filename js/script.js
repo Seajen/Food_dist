0cd9cc00198022c99aae032bc5c04f9817386d94
Seajer <Hupalo.Dima@gmail.com>
2020-09-09 16:01:35 +0300

    added class with tabs

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('DOMContentLoaded', function() {
 
     // Tabs
 
@@ -27,9 +27,9 @@ window.addEventListener('DOMContentLoaded', function () {
     hideTabContent();
     showTabContent();
 
-    tabsParent.addEventListener('click', function (event) {
+    tabsParent.addEventListener('click', function(event) {
         const target = event.target;
-        if (target && target.classList.contains('tabheader__item')) {
+        if(target && target.classList.contains('tabheader__item')) {
             tabs.forEach((item, i) => {
                 if (target === item) {
                     hideTabContent();
@@ -44,11 +44,11 @@ window.addEventListener('DOMContentLoaded', function () {
     const deadline = '2020-05-11';
 
     function getTimeRemaining(endTime) {
-        const t = Date.parse(new Date()) - Date.parse(endTime),
-            days = Math.floor((t / (1000 * 60 * 60 * 24))),
-            seconds = Math.floor((t / 1000) % 60),
-            minutes = Math.floor((t / 1000 / 60) % 60),
-            hours = Math.floor((t / (1000 * 60 * 60) % 24));
+        const t = Date.parse(endTime) - Date.parse(new Date()),
+            days = Math.floor( (t/(1000*60*60*24)) ),
+            seconds = Math.floor( (t/1000) % 60 ),
+            minutes = Math.floor( (t/1000/60) % 60 ),
+            hours = Math.floor( (t/(1000*60*60) % 24) );
 
         return {
             'total': t,
@@ -59,7 +59,7 @@ window.addEventListener('DOMContentLoaded', function () {
         };
     }
 
-    function getZero(num) {
+    function getZero(num){
         if (num >= 0 && num < 10) {
             return '0' + num;
         } else {
@@ -67,7 +67,7 @@ window.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    function setClock(selector, endtime) {
+    function setClock(selector, endTime) {
 
         const timer = document.querySelector(selector),
             days = timer.querySelector("#days"),
@@ -79,7 +79,7 @@ window.addEventListener('DOMContentLoaded', function () {
         updateClock();
 
         function updateClock() {
-            const t = getTimeRemaining(endtime);
+            const t = getTimeRemaining(endTime);
 
             days.innerHTML = getZero(t.days);
             hours.innerHTML = getZero(t.hours);
@@ -94,38 +94,105 @@ window.addEventListener('DOMContentLoaded', function () {
 
     setClock('.timer', deadline);
 
-    //Modal
+    // Modal
 
     const modalTrigger = document.querySelectorAll('[data-modal]'),
         modal = document.querySelector('.modal'),
         modalCloseBtn = document.querySelector('[data-close]');
 
     modalTrigger.forEach(btn => {
-        btn.addEventListener('click', () => {
-
-            document.body.style.overflow = 'hidden';
-            modal.classList.toggle('show');
-        });
+        btn.addEventListener('click', openModal);
     });
 
     function closeModal() {
-        modal.classList.toggle('show');
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
     }
 
     modalCloseBtn.addEventListener('click', closeModal);
 
-
-    modal.addEventListener('click', e => {
+    modal.addEventListener('click', (e) => {
         if (e.target === modal) {
-            closeModal()
+            closeModal();
         }
-    })
+    });
 
-    document.addEventListener('keydown', e => {
-        if (e.code === 'Escape') {
-            closeModal()
+    document.addEventListener('keydown', (e) => {
+        if (e.code === "Escape" && modal.classList.contains('show')) {
+            closeModal();
         }
-    })
+    });
+
+
+
+    // Используем классы для создание карточек меню
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+            element.innerHTML = `
+                <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+            `;
+            this.parent.append(element);
+        }
+    }
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        14,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        21,
+        ".menu .container"
+    ).render();
+
 });
-
-
